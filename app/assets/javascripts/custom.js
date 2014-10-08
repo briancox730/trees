5f91fd4f11012c56893a86a2577c4ea5958ebeff
Brian Cox <briancox730@gmail.com>
2014-10-08 14:19:20 -0400

    new order viewer

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var text = $('#order_tree_id :selected').text().split(' ');
   var type = text[2].toLowerCase();
   $('#tree-display').addClass(type);
-  $('#tree-icon').text(' '+text[0]+'ft.');
+  $('#tree-icon').text(' '+text[0]+'ft. '+text[2]);
 
   $('#order_tree_id').change(function() {
     var text = $('#order_tree_id :selected').text().split(' ');
@@ -10,6 +10,6 @@ $(document).ready(function(){
     $('#tree-display').removeClass('balsam');
     $('#tree-display').removeClass('fraser');
     $('#tree-display').addClass(type);
-    $('#tree-icon').text(' '+text[0]+'ft.');
+    $('#tree-icon').text(' '+text[0]+'ft. '+text[2]);
   })
 });
